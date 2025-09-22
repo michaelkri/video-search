a8dd3056e83c47e1d7fb5d33977f2c0f9ab36603
michaelkri <11300073+michaelkri@users.noreply.github.com>
2025-09-22 15:36:24 +0000

    Moved vector database to a separate file

--- src/app.py
+++ src/app.py
@@ -1,24 +1,17 @@
-import chromadb
-from chromadb.utils.data_loaders import ImageLoader
-from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
+from vector_database import VectorDatabase
 
-client = chromadb.Client()
 
-data_loader = ImageLoader()
-embedding_function = OpenCLIPEmbeddingFunction()
+db = VectorDatabase()
 
-collection = client.create_collection(
-    name='frames',
-    embedding_function=embedding_function,
-    data_loader=data_loader
-)
-
-collection.add(
+db.add(
     ids=['1', '2'],
-    uris=['/workspaces/video-search/images/car.jpg', '/workspaces/video-search/images/house.png']
+    uris=[
+        '/workspaces/video-search/images/car.jpg',
+        '/workspaces/video-search/images/house.png'
+    ]
 )
 
-results = collection.query(
+results = db.query(
     query_texts=['roof'],
     include=['uris', 'distances']
 )
