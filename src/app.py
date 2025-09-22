05501849042c744eeae62de2605b9aea01e2aced
michaelkri <11300073+michaelkri@users.noreply.github.com>
2025-09-22 17:01:53 +0000

    Display search results in a gallery

--- src/app.py
+++ src/app.py
@@ -5,10 +5,12 @@ import gradio as gr
 db = VectorDatabase()
 
 db.add(
-    ids=['1', '2'],
+    ids=['1', '2', '3', '4'],
     uris=[
         '/workspaces/video-search/images/car.jpg',
-        '/workspaces/video-search/images/house.png'
+        '/workspaces/video-search/images/house.png',
+        '/workspaces/video-search/images/bear.jpg',
+        '/workspaces/video-search/images/lake.jpg'
     ]
 )
 
@@ -17,7 +19,9 @@ def search_db(term):
         query_texts=[f'{term}'],
         include=['uris', 'distances']
     )
-    return results
+
+    paths_with_distances = zip(results['uris'][0], map(str, results['distances'][0]))
+    return paths_with_distances
 
 
 with gr.Blocks(theme=gr.themes.Citrus()) as demo:
@@ -26,12 +30,17 @@ with gr.Blocks(theme=gr.themes.Citrus()) as demo:
         search_query_input = gr.Textbox(label="Search Query")
     search_button = gr.Button("Search")
 
-    output_json = gr.JSON(label="Results")
+    output_gallery = gr.Gallery(
+        label="Results",
+        object_fit="cover",
+        columns=4,
+        rows=2
+    )
 
     search_button.click(
         fn=search_db,
         inputs=[search_query_input],
-        outputs=output_json
+        outputs=output_gallery
     )
 
 demo.launch()
